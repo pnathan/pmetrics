9bc0cfeea1a50838562fa4a0639e81dfae13e3b7
Paul Nathan <paul@nathan.house>
2024-02-29 23:19:22 -0800

    a test script

--- curltest.sh
+++ curltest.sh
@@ -1,18 +1,11 @@
 #!/bin/bash -xe
 
-curl -H "X-PMETRICS-API-KEY: a-wiWimWyilf" localhost:1337/api/v1/event
-curl -XPOST -H "X-PMETRICS-API-KEY: a-wiWimWyilf"  localhost:1337/api/v1/event -d  '{"name":"dylos", "dict" : {}}'
-curl -H "X-PMETRICS-API-KEY: a-wiWimWyilf" localhost:1337/api/v1/event
 
-curl -H "X-PMETRICS-API-KEY: a-wiWimWyilf" localhost:1337/api/v1/measure
-curl -XPOST -H "X-PMETRICS-API-KEY: a-wiWimWyilf"  localhost:1337/api/v1/measure -d  '{"name":"dylos", "measurement": 4761.0, "dict" : {}}'
-curl -H "X-PMETRICS-API-KEY: a-wiWimWyilf" localhost:1337/api/v1/measure
+KEY=tyfsOcGoQuetber8
+curl -H "X-PMETRICS-API-KEY: $KEY" localhost:1337/api/v1/event
+curl -XPOST -H "X-PMETRICS-API-KEY: $KEY"  localhost:1337/api/v1/event -d '{"name":"bacon", "dict" : {"they said this was just a test": true}}'
+curl -H "X-PMETRICS-API-KEY: $KEY" localhost:1337/api/v1/event
 
-
-curl -H "X-PMETRICS-API-KEY: a-IbpyucIo" localhost:1337/api/v1/event
-curl -XPOST -H "X-PMETRICS-API-KEY: a-IbpyucIo"  localhost:1337/api/v1/event -d  '{"name":"bacon", "dict" : {"they said this was just a test": true}}'
-curl -H "X-PMETRICS-API-KEY: a-IbpyucIo" localhost:1337/api/v1/event
-
-curl -H "X-PMETRICS-API-KEY: a-IbpyucIo" localhost:1337/api/v1/measure
-curl -XPOST -H "X-PMETRICS-API-KEY: a-IbpyucIo"  localhost:1337/api/v1/measure -d  '{"name":"baker", "measurement": -2.3, "dict" : {"so much data" : 1}}'
-curl -H "X-PMETRICS-API-KEY: a-IbpyucIo" localhost:1337/api/v1/measure
+curl -H "X-PMETRICS-API-KEY: $KEY" localhost:1337/api/v1/measure
+curl -XPOST -H "X-PMETRICS-API-KEY: $KEY"  localhost:1337/api/v1/measure -d  '{"name":"dylos", "measurement": 4761.0, "dict" :  {"so much data" : 1}}'
+curl -H "X-PMETRICS-API-KEY: $KEY" localhost:1337/api/v1/measure
